4818fdd06a96887782c4401d5c8464a388cd6945
maciekdt <74464525+maciekdt@users.noreply.github.com>
2022-03-19 01:52:18 +0100

    firebase service changed from dictionary to class

--- frontend/web-app/src/endpoints/DataBaseService.js
+++ frontend/web-app/src/endpoints/DataBaseService.js
@@ -2,26 +2,27 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { collection, getDocs } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDG_pFjKssAOFtBc19A9eJvEbuNHcQnOYU",
-    authDomain: "test-f0349.firebaseapp.com",
-    projectId: "test-f0349",
-    storageBucket: "test-f0349.appspot.com",
-    messagingSenderId: "110874552011",
-    appId: "1:110874552011:web:a2d27359c97503d5619d4e",
-    measurementId: "G-PTN9MMT7EM"
-};
+class DataBaseService {
+    firebaseConfig = {
+        apiKey: "AIzaSyDG_pFjKssAOFtBc19A9eJvEbuNHcQnOYU",
+        authDomain: "test-f0349.firebaseapp.com",
+        projectId: "test-f0349",
+        storageBucket: "test-f0349.appspot.com",
+        messagingSenderId: "110874552011",
+        appId: "1:110874552011:web:a2d27359c97503d5619d4e",
+        measurementId: "G-PTN9MMT7EM"
+    }
 
-const firebaseApp = initializeApp(firebaseConfig);
-const firestore = getFirestore(firebaseApp);
+    firebaseApp = initializeApp(this.firebaseConfig)
+    firestore = getFirestore(this.firebaseApp)
 
-let service = {
-    test: async function() {
-        let querySnapshot = await getDocs(collection(firestore, "Tasks"));
+
+    async test() {
+        let querySnapshot = await getDocs(collection(this.firestore, "Tasks"));
         querySnapshot.forEach((doc) => {
-            console.log(`${doc.id} => ${doc.data().liftName}`);
-        });
+            console.log(doc.data().liftName);
+        })
     }
 }
 
-export default service;
+export default new DataBaseService();
