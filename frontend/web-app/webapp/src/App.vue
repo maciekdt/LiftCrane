3a01d634ba34aa6aab8efc7c7281e63779211674
ukasz <lukaszkrz@protonmail.com>
2022-05-10 00:05:35 +0200

    Vuetify init

---
+++ frontend/web-app/webapp/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <About/>
+      <Table></Table>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Table from './components/Table';
+import About from './components/About';
+export default {
+  name: 'App',
+
+  components: {
+    About,
+    Table,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
