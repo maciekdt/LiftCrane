f855718ae8faeebbaa0fd577a0ebc9e3ee91ae76
maciekdt <74464525+maciekdt@users.noreply.github.com>
2022-06-04 00:28:04 +0200

    Lifts list activity

---
+++ frontend/android-app/app/src/main/java/com/example/liftcrane/ui/liftslist/LiftsListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.liftcrane.ui.liftslist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.liftcrane.R
+
+class LiftsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lifts_list)
+    }
+}
---
+++ frontend/android-app/app/src/main/java/com/example/liftcrane/ui/liftslist/LiftsListAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.liftcrane.ui.liftslist
+
+class LiftsListAdapter {
+}
