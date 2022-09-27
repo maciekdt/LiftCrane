e6b5eb6e8a05c1120a29296325a6078adf871cee
maciekdt <74464525+maciekdt@users.noreply.github.com>
2022-09-27 18:28:41 +0200

    First version

---
+++ frontend/android-app/app/src/main/java/com/example/liftcrane/ui/imgpreview/ImagePreviewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.liftcrane
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImagePreviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_preview)
+    }
+}
